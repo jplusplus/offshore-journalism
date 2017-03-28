af1d24e18541b6395c277d1e0c94148946878898
pirhoo <hello@pirhoo.com>
2017-03-28 15:16:52 +0200

    Changed typo

--- src/app/containers/main/main.js
+++ src/app/containers/main/main.js
@@ -1,7 +1,6 @@
 export const main = {
   template: require('./main.html'),
   controller() {
-    this.intro = require('../../../data/page-intro.json');
     this.structure = require('../../../data/structure.json');
     this.categories = [
       {title: "Intro", slug: 'intro'},
