283c7deb6d00e1f15f5afbbb8cbabff03a4e7d9c
pirhoo <hello@pirhoo.com>
2017-03-28 12:20:33 +0200

    Updated structure

--- src/app/containers/main/main.js
+++ src/app/containers/main/main.js
@@ -3,5 +3,10 @@ export const main = {
   controller() {
     this.intro = require('../../../data/page-intro.json');
     this.structure = require('../../../data/structure.json');
+    this.categories = [
+      {title: "Intro", slug: 'intro'},
+      {title: "Case studies", slug: 'case-studies'},
+      {title: "Solutions", slug: 'solutions'}
+    ];
   }
 };
